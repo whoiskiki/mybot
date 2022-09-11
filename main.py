e54cb6f265597fa9bf9b579145bc4c8b888f0640
whoiskiki <112754303+whoiskiki@users.noreply.github.com>
2022-09-11 12:35:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ logger = telebot.logger
 logger.setLevel(10)  # set the level on the debug/ allows us to see debugging messages on the heroku
 
 
-if __name__ == "__main__":  # guarantee that server will be working only with main-script (webhooks)
+if __name__ == "main":  # guarantee that server will be working only with main-script (webhooks)
     botToken.remove_webhook()
     botToken.set_webhook(url=APP_URL)
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))  # makes the server public
